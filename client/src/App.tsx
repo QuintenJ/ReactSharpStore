6202fd8e51479f2a233ca3abf0c578432adc034e
Quinten <me@quintenjames.com>
2022-01-07 09:14:52 -0700

    wip-laptoppsuh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const products = [
+  { name: 'Product 1', price: 100.00 },
+  { name: 'Product 2', price: 200.00 },
+  { name: 'Product 2', price: 200.00 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactSharpStore</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
