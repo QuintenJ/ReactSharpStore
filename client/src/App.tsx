35535a77512d739cdb253e42c6c952250f4e0d40
Quinten <me@quintenjames.com>
2022-01-08 00:57:04 -0700

    Cosume/Render API to Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-const products = [
-  { name: 'Product 1', price: 100.00 },
-  { name: 'Product 2', price: 200.00 },
-  { name: 'Product 2', price: 200.00 },
-];
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'Product 1', price: 100.00 },
+    { name: 'Product 2', price: 200.00 },
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+    { name: 'Product ' + (prevState.length + 1), price: (prevState.length * 100 + 100) }]);
+  }
   return (
     <div>
       <h1>ReactSharpStore</h1>
@@ -13,6 +24,7 @@ function App() {
           <li key={index}>{item.name} - {item.price}</li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
