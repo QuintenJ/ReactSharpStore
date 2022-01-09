72d99c381c11ee9a6e291309362c5eb1f31dc269
Quinten James <Me@QuintenJames.com>
2022-01-09 15:50:50 -0700

    Product Cards/UI AppBar DarkMode (#5)
    Create Product Cards UI Layout. Darkmode with toggle. Appbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,34 +1,33 @@
-import { Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
+import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? "#eaeaea" : "#212121"
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: prevState.length + 101,
-      name: 'Product ' + (prevState.length + 1),
-      price: (prevState.length * 100 + 100),
-      brand: 'Brand Test',
-      description: 'Description Test',
-      pictureUrl: 'https://picsum.photos/200',
-    }]);
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
   return (
-    <>
-      <Typography variant="h1">ReactSharpStore</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 
