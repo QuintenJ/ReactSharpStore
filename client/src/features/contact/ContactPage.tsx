85847e15552b76400e68dddd7bbf85ba19879cda
Quinten <me@quintenjames.com>
2022-01-10 22:10:03 -0700

    Add NavLinks & Shopping Link To AppBar

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 export default function ContactPage() {
     return (
         <Typography variant="h2">
-            ReactSharpStore Contact Page
+            Contact Page
         </Typography>
     );
 }
