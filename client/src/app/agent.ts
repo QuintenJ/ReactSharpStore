97a7ba3be7c50f4c7996526ae08cafaceda3bcf4
Quinten James <Me@QuintenJames.com>
2022-01-12 01:42:44 -0700

    Error Handling Setup/Error Test Page (#8)
    * Error Handling Setup Test Controller
    
    * Server Side ErrorHandling Middleware
    
    * Centralise Axios Requests
    
    * Axios Interceptors To Catch And Handle Errors

---
+++ client/src/app/agent.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+axios.defaults.baseURL = 'http://localhost:5000/api/';
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    console.log("Caught By Axios Interceptor");
+    return Promise.reject(error.response);
+});
+
+const requests = {
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    delete: (url: string) => axios.delete(url).then(responseBody),
+}
+
+const Catalog = {
+    list: () => requests.get('products'),
+    details: (id: number) => requests.get(`products/${id}`)
+}
+
+const testErrors = {
+    get400Error: () => requests.get('errortest/bad-request'),
+    get401Error: () => requests.get('errortest/unauthorized'),
+    get404Error: () => requests.get('errortest/not-found'),
+    get500Error: () => requests.get('errortest/server-error'),
+    getValidationError: () => requests.get('errortest/validation-error'),
+}
+const agent = {
+    Catalog,
+    testErrors
+}
+
+export default agent;
