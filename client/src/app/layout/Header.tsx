c52e1da8ae6ec6f58bde88d487a82bb9ffbc1507
Quinten <me@quintenjames.com>
2022-01-12 21:54:51 -0700

    Add Toast Notifications For Errors

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { NavLink } from "react-router-dom";
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 
 interface Props {
     darkMode: boolean;
@@ -44,6 +45,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                         ReactSharpStore
                     </Typography>
                     <Switch checked={darkMode} onChange={handleThemeChange} />
+                    <DarkModeOutlinedIcon />
                 </Box>
 
 
@@ -61,7 +63,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                 </List>
                 <Box display="flex" alignItems="center">
                     <IconButton size="large" sx={{ color: "inherit" }}>
-                        <Badge badgeContent={4} color="secondary">
+                        <Badge badgeContent={4} color="error">
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
