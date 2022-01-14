8d7e24d6bfacdd1f7c1e87d1b08ddac6953bab0e
Quinten James <Me@QuintenJames.com>
2022-01-13 18:48:53 -0700

    Simulate Load Delay/Loading Component (#10)

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
@@ -17,7 +18,7 @@ export default function ProductDetails() {
             .finally(() => setLoading(false));
     }, [id])
 
-    if (loading) return <Typography variant="h3">Loading...</Typography>
+    if (loading) return <LoadingComponent message="Loading Product..." />;
 
     if (!product) return <NotFound />
 
