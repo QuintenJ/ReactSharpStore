a6795d940d14618a6cab11a2804d5b0664f0b748
Quinten <me@quintenjames.com>
2022-01-15 13:19:34 -0700

    Style CartPage Display API Data

--- client/src/features/cart/CartPage.tsx
+++ client/src/features/cart/CartPage.tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
@@ -20,6 +21,38 @@ export default function CartPage() {
     if (!cart) return <Typography variant="h3">Your Cart Is Empty</Typography>
 
     return (
-        <h1>Buyer Id = {cart.buyerId}</h1>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Subtotal</TableCell>
+                        <TableCell align="right"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {cart.items.map(item => (
+                        <TableRow
+                            key={item.productId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {item.name}
+                            </TableCell>
+                            <TableCell align="right">${item.price.toFixed(2)}</TableCell>
+                            <TableCell align="right">{item.quantity}</TableCell>
+                            <TableCell align="right">${(item.price * item.quantity).toFixed(2)}</TableCell>
+                            <TableCell align="right">
+                                <IconButton color="error">
+                                    <Delete />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
